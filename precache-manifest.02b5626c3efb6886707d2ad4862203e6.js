248d30a74e98eb20773918630d9893bf2b03b6bb
Kabir Hasan <kabir.webdev@gmail.com>
2021-09-06 12:01:38 +0600

    Updates

---
+++ precache-manifest.02b5626c3efb6886707d2ad4862203e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f05bcb82fa1b2f9bc818eefe3c35af1c",
+    "url": "/knowledge_gate/index.html"
+  },
+  {
+    "revision": "75a6ee8894ce6cd73bbe",
+    "url": "/knowledge_gate/static/css/main.d8e6437c.chunk.css"
+  },
+  {
+    "revision": "e4a543e8d3995b379607",
+    "url": "/knowledge_gate/static/js/2.d09d21bf.chunk.js"
+  },
+  {
+    "revision": "8f986cbc485d190eb93a96e5320228fa",
+    "url": "/knowledge_gate/static/js/2.d09d21bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75a6ee8894ce6cd73bbe",
+    "url": "/knowledge_gate/static/js/main.7eef1a31.chunk.js"
+  },
+  {
+    "revision": "d9e8e079a0a19480f27f",
+    "url": "/knowledge_gate/static/js/runtime-main.29a6e8e0.js"
+  }
+]);
